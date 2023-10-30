9bdb01211f5066df610acdb36198c7fe08e1ed3a
Karthik1812003 <147533677+Karthik1812003@users.noreply.github.com>
2023-10-30 09:01:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const inputbox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addtask() {
+    if (inputbox.value.trim() === '') {
+        alert("You must write something");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+}, false);
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
+
+
